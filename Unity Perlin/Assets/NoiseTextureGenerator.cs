9d6f2e2f5dd9b948a2b77603254cbc140c5628e1
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2018-10-12 15:15:38 -0400

    It lives!

---
+++ Unity Perlin/Assets/NoiseTextureGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseTextureGenerator : MonoBehaviour {
+
+    public int height = 256;
+    public int width = 256;
+
+    public float scale;
+    public PerlinGenerator pg;
+
+    private void Start()
+    {
+        Renderer r = GetComponent<Renderer>();
+
+        r.material.mainTexture = GenerateTexture();
+    }
+
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float)x / width;
+        float yCoord = (float)y / height;
+
+        float val = pg.GetPerlin(xCoord, yCoord, 0.0f);
+        return new Color(val, val, val);
+    }
+}
--- Unity Perlin/Assets/PerlinGenerator.cs
+++ Unity Perlin/Assets/PerlinGenerator.cs
@@ -10,6 +10,8 @@ public class PerlinGenerator : MonoBehaviour {
     private static int permutationRepeats = 2;
     private static int permutationLength = 256;
 
+    [SerializeField] private int repeat = -1;
+    [SerializeField] private float scale = 20.0f;
 
     static PerlinGenerator()
     {
@@ -49,19 +51,114 @@ public class PerlinGenerator : MonoBehaviour {
         }
     }
 
-    private void Start()
+    private static float PerlinFade(float t)
     {
-        string str = "";
+        //Ken Perlin's fade function. (6t^5 - 15t^4 + 10t^3)
+        return t * t * t * (t * (t * 6 - 15) + 10);
+    }
 
-        for(int i = 0; i < permutation.Length; i++)
+    private static float GetGradient(int hash, float x, float y, float z)
+    {
+        switch (hash & 0xF)
         {
-            str += permutation[i].ToString();
-            if(i != permutationLength - 1)
-            {
-                str += ", ";
-            }
+            case 0x0: return x + y;
+            case 0x1: return -x + y;
+            case 0x2: return x - y;
+            case 0x3: return -x - y;
+            case 0x4: return x + z;
+            case 0x5: return -x + z;
+            case 0x6: return x - z;
+            case 0x7: return -x - z;
+            case 0x8: return y + z;
+            case 0x9: return -y + z;
+            case 0xA: return y - z;
+            case 0xB: return -y - z;
+            case 0xC: return y + x;
+            case 0xD: return -y + z;
+            case 0xE: return y - x;
+            case 0xF: return -y - z;
+            default: return 0; // never happens
+        }
+    }
+
+    public float GetSteppedPerlin(float x, float y, float z, float step)
+    {
+        float noise = GetPerlin(x, y, z);
+
+        if(noise >= step) { noise = 1; }
+        else { noise = 0; }
+
+        return noise;
+    }
+
+    public float GetPerlin(float x, float y, float z)
+    {
+        //Handle scale
+        x *= scale;
+        y *= scale;
+        z *= scale;
+
+        //Handle repeat overflows in the coordinates.
+        if(repeat > 0)
+        {
+            x = x % repeat;
+            y = y % repeat;
+            z = z % repeat;
         }
 
-        Debug.Log(str);
+        //Establish unit cube coords. Bind to 0-255 to avoid overflow errors
+        int cubeX = (int)x & 255;
+        int cubeY = (int)y & 255;
+        int cubeZ = (int)z & 255;
+        //Get location within cube unit.
+        float localX = x - (int)x;
+        float localY = y - (int)y;
+        float localZ = z - (int)z;
+
+        float u = PerlinFade(localX);
+        float v = PerlinFade(localY);
+        float w = PerlinFade(localZ);
+
+        //Hash the eight coordinates of the Unit Cube found above.
+        int p1, p2, p3, p4, p5, p6, p7, p8;
+
+        p1 = permutation[permutation[permutation[cubeX] + cubeY] + cubeZ];
+        p2 = permutation[permutation[permutation[cubeX] + Increment(cubeY)] + cubeZ];
+        p3 = permutation[permutation[permutation[cubeX] + cubeY] + Increment(cubeZ)];
+        p4 = permutation[permutation[permutation[cubeX] + Increment(cubeY)] + Increment(cubeZ)];
+        p5 = permutation[permutation[permutation[Increment(cubeX)] + cubeY] + cubeZ];
+        p6 = permutation[permutation[permutation[Increment(cubeX)] + Increment(cubeY)] + cubeZ];
+        p7 = permutation[permutation[permutation[Increment(cubeX)] + cubeY] + Increment(cubeZ)];
+        p8 = permutation[permutation[permutation[Increment(cubeX)] + Increment(cubeY)] + Increment(cubeZ)];
+
+        float x1, y1, x2, y2;
+
+        x1 = Mathf.Lerp(GetGradient(p1, localX, localY, localZ), GetGradient(p5, localX - 1, localY, localZ), u);
+        x2 = Mathf.Lerp(GetGradient(p2, localX, localY - 1, localZ), GetGradient(p6, localX - 1, localY - 1, localZ), u);
+
+        y1 = Mathf.Lerp(x1, x2, v);
+
+        x1 = Mathf.Lerp(GetGradient(p3, localX, localY, localZ - 1), GetGradient(p7, localX - 1, localY, localZ - 1), u);
+        x2 = Mathf.Lerp(GetGradient(p4, localX, localY - 1, localZ - 1), GetGradient(p8, localX - 1, localY - 1, localZ - 1), u);
+
+        y2 = Mathf.Lerp(x1, x2, v);
+
+        return (Mathf.Lerp(y1, y2, w) + 1) * 0.5f;
+
     }
+
+    private int Increment(int num)
+    {
+        num++;
+        if(repeat > 0) { num %= repeat; }
+
+        return num;
+    }
+
+
+    private void Start()
+    {
+
+    }
+
 }
